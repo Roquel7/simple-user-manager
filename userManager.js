86fade04e6902094d6acc57cb4a4edf7ebe8cf5e
Roquel7 <Roquel7@github.com>
2020-09-05 16:10:37 -0600

    added function to write a json file with the users

--- userManager.js
+++ userManager.js
@@ -4,7 +4,7 @@ const path = require('path')
 const port = 4000
 const fs = require('fs')
 const { v4: uuidv4 } = require('uuid')
-// const studentList = fs.createReadStream('./data/students.json')
+const studentList = fs.createReadStream('./data/students.json')
 
 
 
@@ -13,10 +13,10 @@ const dbConnectionString= 'mongodb://localhost:27017/userManager';
 mongoose.connect(dbConnectionString, {useNewUrlParser: true,useUnifiedTopology: true,}); 
 const db = mongoose.connection; //connecting to the Database
 
-
 const userSchema = new mongoose.Schema({
 
-    full_name: String,
+    first_name: String,
+    last_name: String,
     id: mongoose.Mixed,
     age: { type: Number, min: 1, max: 99 },
     email: String,
@@ -33,8 +33,6 @@ db.on('connecting', () => console.log('Connecting'))
 db.once('open', () => console.log('Connected, database is up'))
 
 
-
-
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
@@ -49,10 +47,15 @@ app.get('/', (req, res) => {
     })
 })
 
+app.get('/listOfStudents', (req,res) => {
+    fs.createReadStream('data/students.json').pipe(res)
+ })
+
 app.post('/addingStudent', (req, res) => {
 
     let user = new User() 
-        user.full_name =  req.body.full_name;
+        user.first_name =  req.body.first_name;
+        user.last_name =  req.body.last_name;
         user.id = uuidv4();
         user.markModified('id');
         user.age = req.body.age;
@@ -62,6 +65,23 @@ app.post('/addingStudent', (req, res) => {
                 console.log(err)
             } 
             res.redirect('/studentList')
+        })
+
+        const info = require('./data/students.json')
+
+        let userInfo = {
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            id: Math.floor(Math.random() * 100),
+            age: req.body.age,
+            email: req.body.email
+        }    
+    
+        info.push(userInfo)
+    
+        fs.writeFile('./data/students.json', JSON.stringify(info), (err) => {
+            if (err) throw err;
+    
         })
 })
 
@@ -89,9 +109,9 @@ app.get('/editUser/:edit', async (req, res) => {
 
 app.post('/editUser/:edit', async (req, res) => {
 
-    const {full_name: newName, age: newAge, email: newEmail} = req.body
+    const {first_name: newFirstName, last_name: newLastName, age: newAge, email: newEmail} = req.body
     
-    await User.updateOne({id:req.params.edit}, {full_name: newName, age: newAge, email: newEmail})
+    await User.updateOne({id:req.params.edit}, {first_name: newFirstName, last_name: newLastName, age: newAge, email: newEmail})
         .then(
             (response) => {
                 console.log('update complete', response);
